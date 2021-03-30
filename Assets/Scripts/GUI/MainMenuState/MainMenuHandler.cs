7f0700b4baee37082122de48add6a44a05b5cb75
nosrick <nosrick@gmail.com>
2021-03-30 16:36:27 +0100

    Trying to get the MainMenu working properly. Taking out the background creation removes the background.

--- Assets/Scripts/GUI/MainMenuState/MainMenuHandler.cs
+++ Assets/Scripts/GUI/MainMenuState/MainMenuHandler.cs
@@ -6,24 +6,6 @@ namespace JoyLib.Code.Unity.GUI.MainMenuState
 {
     public class MainMenuHandler : GUIData
     {
-        public override void _Ready()
-        {
-            ManagedUIElement background = new ManagedUIElement
-            {
-                AnchorBottom = 1,
-                AnchorRight = 1,
-                ElementName = "DefaultWindow"
-            };
-
-            //ISpriteState state = GlobalConstants.GameManager.GUIManager.UISprites["DefaultWindow"];
-            
-            this.AddChild(background);
-            this.MoveChild(background, 0);
-            
-            //background.AddSpriteState(state);
-            //GlobalConstants.GameManager.GUIManager.SetupManagedComponents(this);
-        }
-
         public void NewGame()
         {
             GlobalConstants.GameManager.GUIManager.CloseAllGUIs();
